05f16554d0c5c912f0cc17ba4713b6fabc6348b3
benjaminhuanghuang <bhuang@rms.com>
2021-03-30 14:38:44 -0700

    update

---
+++ README.md
@@ -0,0 +1,3 @@
+# Data Science for Java Developers
+https://www.linkedin.com/learning/data-science-for-java-developers/displaying-a-scatterplot
+
---
+++ knn/DataItem.java
@@ -0,0 +1,6 @@
+import java.util.List;
+
+public interface DataItem {
+    public List<Double> getCoordinates();
+    public String getClassifier();
+}
